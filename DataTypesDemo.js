00657b041e4e5947ede6c8203b210d4576d32fe1
Mihir Kothari <kotharimihir07@gmail.com>
2025-08-27 16:16:18 +0530

    DataaTypes Demo

---
+++ DataTypesDemo.js
@@ -0,0 +1,29 @@
+"use strict"; // Treat all the js code as a newer version
+
+// alert(3+3)  // It will give error as we are using nodejs not browser
+
+/* 
+
+1 .  Primitive (single value)
+
+1. String → text (inside " " or ' ' or ` `) 
+2. Number → integers or decimals
+3. Boolean → true or false
+4. Undefined → variable declared but not given value  
+ => let x;
+ console.log(x); // undefined
+
+5. Null → intentional empty value   
+ => let data = null;
+
+6. Symbol (ES6) → unique value
+7. BigInt → very large numbers
+
+2  .  Non-Primitive (reference types)
+
+8. Object → collection of key–value pairs
+
+*/
+
+let name = "Mihir Kothari"
+console.log(typeof name);
