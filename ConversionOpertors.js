78403ae4672d1357391d4109716bcd34e9358feb
Mihir Kothari <kotharimihir07@gmail.com>
2025-08-27 16:34:28 +0530

    Conversion of datatypes

---
+++ ConversionOpertors.js
@@ -0,0 +1,20 @@
+let score = "18"
+
+console.log(score);
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+/* 
+
+33 => 33
+"33" => 33
+"33abc" => NaN (not a number)
+true => 1  , false => 0
+
+*/
+
